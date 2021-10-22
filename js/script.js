2edd96e54ef8e1aadb8b75a693b4e332828307af
danilo155 <playgroop@ukr.net>
2021-10-22 19:32:33 +0300

    доробка

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
- function onClickSlider() {
-     document.body.classList.toggle('show-nav')
+let width = document.body.clientWidth;
+let newWidth = (width - 200).toString() + 'px';
 
+console.log(newWidth)
+
+let i = 0;
+
+function onClickSlide() {
+    document.body.classList.toggle('show-nav');
+    document.body.classList.toggle('width-second-position');
+    document.querySelector('header').classList.toggle('border-active');
+    if (i == 0) {
+        document.querySelector('header').style.width = newWidth;
+        document.querySelector('main').style.width = newWidth;
+        document.querySelector('body').style.width = newWidth;
+        i++;
+    } else if (i == 1) {
+        document.querySelector('header').style.width = width.toString() + 'px';
+        document.querySelector('main').style.width = width.toString() + 'px';
+        document.querySelector('body').style.width = width.toString() + 'px';
+        i--;
+    }
 }
